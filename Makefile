cd02c3a8585a0d60a018b1fcf5bda7708a8b0dd0
Martijn Koedam <m.l.p.j.koedam@tue.nl>
2011-12-20 09:45:21 +0100

    Add License. Use clang (if possible), support multiple input files.

--- Makefile
+++ Makefile
@@ -1,8 +1,15 @@
 SOURCE=$(wildcard *.c)
 PROGRAM=file2bin
+EMPTY=
 
+ifeq ($(shell which clang),$(empty))
+$(info * Using default compiler)
+else
+$(info * Using clang compiler)
+	CC = $(shell which clang)
+endif
 
-CFLAGS+=-Wall -Werror -g3
+CFLAGS+=-Wextra -Wall -Werror -g3 -std=c99 -D_GNU_SOURCE
 
 all: $(PROGRAM)
 
--- file2bin.c
+++ file2bin.c
@@ -1,11 +1,11 @@
-/* file2bin.c: Converts a file to a binary starting with size (32bit) and padded to whole words (32 bit). 
+/* file2bin.c: Converts file(s) to a binary stream starting with size (32bit unsigned int)
+ * and padded to whole words (32 bit). 
  *
  * Copyright (C) 2011 Martijn Koedam 
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * the Free Software Foundation; version 2 of the License.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,6 +15,16 @@
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program was written for use with the CompSoC platform (TU/e, TUd).
+ */
+
+/***
+ * Output format:
+ * 32bit unsigned int with size of binary data (excluding itself).
+ * <file 1> padded to 4bytes (32 bits, one word)
+ * <file ...> 
+ * <file n> padded to 4bytes (32 bits, one word)
  */
 
 #include <stdio.h>
@@ -34,90 +44,140 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+// uint32_t and endian conversion
 #include <stdint.h>
+#include <endian.h>
 
 // The maximum size we support. Currently 16MB should be sufficient.
-const int MAX_SIZE = 1024*1024*16;
+const unsigned int MAX_SIZE = 1024*1024*16;
 
 
 // The output is rounded up to complete words.
 // Padding is added as zeros. 
 // On the target platform memory can only be accessed in words.
-const int WORD_SIZE = 4;
+const unsigned int WORD_SIZE = 4;
 
 
 int main ( int argc , char **argv )
 {
-	if(argc != 2) {
+	// Total size we are going transmit (without size header)
+	uint32_t total_size = 0;
+	unsigned int number_files = argc;
+
+	// Check number of arguments.
+	if(argc < 2) {
 		fprintf(stderr, "%s: requires 1 input file.\n", argv[0]);
 		return EXIT_FAILURE;
 	}
 
-
-	// Get file size 
-	struct stat file_stat;
-	int retv = stat(argv[1], &file_stat);
-
-	// Error
-	if(retv != 0)
+	for(unsigned int current_file = 1; current_file < number_files;current_file++)
 	{
-		fprintf(stderr, "%s: Failed to open input file: %s, reason: %s\n", 
-				argv[0],
-				argv[1],
-				strerror(errno));
-		return EXIT_FAILURE;
+		// Get file size 
+		struct stat file_stat;
+		int retv = stat(argv[current_file], &file_stat);
+
+		// Check help message.
+		if(strcmp(argv[current_file], "-h") == 0 || strcmp(argv[current_file], "--help")==0)
+		{
+			fprintf(stderr, "Usage: %s <input files>\n\n", argv[0]);
+			fprintf(stderr, "%s packs all given input files into one binary stream. Each file is padded to be word aligned.\n", argv[0]);
+			fprintf(stderr, "Prepended is the size of the stream in a 32bit little endian unsigned int.\n");
+			return EXIT_SUCCESS;
+		}
+
+		// Error
+		if(retv != 0)
+		{
+			fprintf(stderr, "%s: Failed to open input file: %s, reason: %s\n", 
+					argv[0],
+					argv[current_file],
+					strerror(errno));
+			return EXIT_FAILURE;
+		}
+		// Check read permission.
+		// size
+		if(file_stat.st_size > MAX_SIZE) 
+		{
+			fprintf(stderr, "%s: File '%s' size is to big: %u bytes (limit %u bytes)\n", 
+					argv[0],
+					argv[current_file],
+					(unsigned int) file_stat.st_size,
+					MAX_SIZE);
+			return EXIT_FAILURE;
+		}
+		uint32_t file_size = file_stat.st_size;
+		uint32_t padding = file_size%WORD_SIZE;
+		// Calculate padding 
+
+		padding = (padding > 0)?(WORD_SIZE-padding):0;
+		total_size += file_size+padding;
+
+		fprintf(stderr, "File size: %u, padding: %u, total: %u\n", file_size, padding, total_size);
 	}
-	// Check read permission.
-	// size
-	if(file_stat.st_size > MAX_SIZE) 
+	// Check total size
+	if(total_size > MAX_SIZE) {
+			fprintf(stderr, "%s: File size is to big: %u bytes (limit %u bytes)\n", 
+					argv[0],
+					(unsigned int)total_size, 
+					MAX_SIZE);
+			return EXIT_FAILURE;
+	}
+
+	// Explicitely convert to little endian.	
+	uint32_t le_tsz = htole32(total_size);
+	// Write out total file size.
+	fwrite((void*)&le_tsz, sizeof(uint32_t),1,stdout);
+
+	for(unsigned int current_file = 1; current_file < number_files;current_file++)
 	{
-		fprintf(stderr, "%s: File size is to big: %u bytes (limit %u bytes)\n", 
-				argv[0],
-				(unsigned int) file_stat.st_size,
-				MAX_SIZE);
-		return EXIT_FAILURE;
+		// Get file size 
+		struct stat file_stat;
+		int retv = stat(argv[current_file], &file_stat);
+
+		// Error
+		if(retv != 0)
+		{
+			fprintf(stderr, "%s: Failed to open input file: %s, reason: %s\n", 
+					argv[0],
+					argv[current_file],
+					strerror(errno));
+			return EXIT_FAILURE;
+		}
+		// Check read permission.
+		uint32_t file_size = file_stat.st_size;
+		uint32_t padding = file_size%WORD_SIZE;
+		// Calculate padding 
+		padding = (padding > 0)?(WORD_SIZE-padding):0;
+
+		// Read the file.
+		int fd = open(argv[current_file], O_RDONLY);
+
+		// Read bytewise.
+		unsigned char d;
+		while(read(fd, &d,1) == 1)
+		{ 
+			putc(d, stdout);
+			file_size--;
+		}
+
+		// Check if complete file was written.
+		if(file_size > 0)
+		{
+			// Read data does not match up.
+			fprintf(stderr, "%s:  Failed to read complete file, %u bytes remaining.\n", argv[0], file_size); 
+			close(fd);	
+			return EXIT_FAILURE;
+		}
+
+		// Write padding.
+		while(padding) {
+			putc(0, stdout);
+			padding--;
+		}
+
+		// Close
+		close(fd);
 	}
-	uint32_t file_size = file_stat.st_size;
-	uint32_t padding = file_size%WORD_SIZE;
-	// Calculate padding 
-
-	padding = (padding > 0)?(WORD_SIZE-padding):0;
-	uint32_t total_size = file_size+padding;
-
-	fprintf(stderr, "File size: %u, padding: %u, total: %u\n", file_size, padding, total_size);
-
-	// Write out file size.
-	fwrite((void*)&total_size, sizeof(uint32_t),1,stdout);
-
-	// Read the file.
-	int fd = open(argv[1], O_RDONLY);
-
-	// Read bytewise.
-	unsigned char d;
-	while(read(fd, &d,1) == 1)
-	{ 
-		putc(d, stdout);
-		file_size--;
-	}
-
-	// Check if complete file was written.
-	if(file_size > 0)
-	{
-		// Read data does not match up.
-		fprintf(stderr, "%s:  Failed to read complete file, %u bytes remaining.\n", argv[0], file_size); 
-		close(fd);	
-		return EXIT_FAILURE;
-	}
-
-	// Write padding.
-	while(padding) {
-		putc(0, stdout);
-		padding--;
-	}
-
-	// Close
-	close(fd);
-
 	// Return success
 	return EXIT_SUCCESS;
 }
